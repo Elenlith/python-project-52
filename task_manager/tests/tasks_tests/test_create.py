0e512b1214b1a976713b4ab897e1ac261cf3aa5d
Elenlith <mishchenkov@list.ru>
2023-05-16 12:55:57 +0300

    add tests for Tasks CRUD

---
+++ task_manager/tests/tasks_tests/test_create.py
@@ -0,0 +1,31 @@
+from task_manager.users.models import User
+from task_manager.statuses.models import  Status
+from task_manager.tasks.models import Task
+from django.urls import reverse_lazy as reverse
+from django.test import TestCase
+
+
+class Create(TestCase):
+    fixtures = ['db_task.json']
+
+    def test_create_open_without_login(self):
+        response = self.client.get(reverse('task_create'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_create_task(self):
+        user = User.objects.all().first()
+        self.client.force_login(user=user)
+        status = Status.objects.all().first()
+        response = self.client.get(reverse('task_create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Task.objects.all().count(), 1)
+        response = self.client.post(
+            reverse('task_create'),
+            {'name': 'test task',
+             'author': user.id,
+             'status': status.id,
+             'executor': user.id
+             }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Task.objects.all().count(), 2)
