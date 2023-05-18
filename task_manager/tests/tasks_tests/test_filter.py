674a6941806334db7b0131977d8d0c61614b699a
Elenlith <mishchenkov@list.ru>
2023-05-18 16:26:54 +0300

    add Tasks filters tests

---
+++ task_manager/tests/tasks_tests/test_filter.py
@@ -0,0 +1,39 @@
+from task_manager.users.models import User
+from django.urls import reverse_lazy as reverse
+from django.test import TestCase
+
+
+class TestFilter(TestCase):
+    fixtures = [
+        'db.json',
+        'db_status.json',
+        'db_tasks.json',
+        'db_labels.json'
+    ]
+
+    def test_filter_by_status(self):
+        user = User.objects.all().first()
+        self.client.force_login(user=user)
+        self.client.get(reverse('tasks_list'))
+        result = self.client.get(reverse('tasks_list'), {'status': '1'})
+        self.assertContains(result, 'Task 1')
+        self.assertNotContains(result, 'Task 2')
+        self.assertContains(result, 'Task 3')
+
+    def test_filter_by_executor(self):
+        user = User.objects.all().first()
+        self.client.force_login(user=user)
+        self.client.get(reverse('tasks_list'))
+        result = self.client.get(reverse('tasks_list'), {'executor': '2'})
+        self.assertContains(result, 'Task 1')
+        self.assertNotContains(result, 'Task 2')
+        self.assertNotContains(result, 'Task 3')
+
+    def test_filter_by_label(self):
+        user = User.objects.all().first()
+        self.client.force_login(user=user)
+        self.client.get(reverse('tasks_list'))
+        result = self.client.get(reverse('tasks_list'), {'labels': '2'})
+        self.assertContains(result, 'Task 1')
+        self.assertContains(result, 'Task 2')
+        self.assertNotContains(result, 'Task 3')
