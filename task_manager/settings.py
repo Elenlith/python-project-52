e83c8519cb1f3890212089b1895e5b97dc6a60a9
Elenlith <mishchenkov@list.ru>
2023-05-23 15:30:00 +0300

    update database settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -93,21 +93,16 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        },
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=1800,
-        )
-    }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgresql': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=1800,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
